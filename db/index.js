a6177b4358f4f6c4cd73cb2542cff2671ce99917
Juan Briceno <juanbriceno7@gmail.com>
2022-10-01 11:53:21 -0700

    Completed "Day 3" of server workshop

--- db/index.js
+++ db/index.js
@@ -73,6 +73,13 @@ async function getPostById(postId) {
       WHERE id=$1;
     `, [postId]);
 
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+
     const { rows: tags } = await client.query(`
       SELECT tags.*
       FROM tags
@@ -344,6 +351,7 @@ module.exports = {
     getAllTags,
     getPostsByTagName,
     getUserById,
+    getPostById,
     getUserByUsername,
     updateUser,
     updatePost,
